998d8a4d4a967a8d34a9dac8ac7f331fb3692950
HSgod <hsgodkontakt@gmail.com>
2020-07-04 21:28:34 +0000

    raphael: Add aptX props

--- system_prop.mk
+++ system_prop.mk
@@ -1,6 +1,8 @@
 # Bluetooth
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.bt.a2dp.aac_whitelist=false \
+    persist.bt.enableAptXHD=true \
+    persist.service.btui.use_aptx=1 \
     persist.vendor.btstack.a2dp_offload_cap=sbc-aptx-aptxtws-aptxhd-aptxadaptive-aac-ldac \
     persist.vendor.btstack.enable.splita2dp=true \
     persist.vendor.btstack.enable.twsplus=true \
